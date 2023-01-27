56f68b1bc0070a4ba8e22bba7d3733c58561396f
JOSE ALARCON MOLINA <jos3.4larc0n@gmail.com>
2023-01-27 00:40:29 -0500

    add opacity class to the css file it was not defined in the starter code

--- assets/js/paint.js
+++ assets/js/paint.js
@@ -1,72 +1,106 @@
-// Declare pricing information for each paint color
-const paintPrices = {
-    'Lime Green': 14.99,
-    'Medium Brown': 11.14,
-    'Royal Blue': 22.99,
-    'Solid Black': 4.99,
-    'Solid Cyan': 8.22,
-    'Solid Purple': 11.99,
-    'Solid Red': 13.42,
-    'Solid White': 21.98,
-    'Solid Yellow': 14.99
-};
-
-// Function to configure event listeners for image elements
 function configureListeners() {
-    // Select all img elements
-    let images = document.querySelectorAll("img");  
-    // Iterate through each image element
+    let images = document.querySelectorAll("img");  // select img elements  
     images.forEach(function(image) {
-        // Add mouseover event listener
+        // add mouseover event listener
         image.addEventListener("mouseover", addOpacity);
-        // Add mouseout event listener
+        // add mouseout event listener
         image.addEventListener("mouseout", removeOpacity);
     });
 }
 
-// Function to add opacity to image when hovered over
+
+// function to add opacity and display product information when image is hovered over
 function addOpacity(event) {
-    // Add appropriate CSS class
+    // add "opacity" CSS class to hovered over image
     event.target.classList.add("opacity");
-    // Get product information for hovered over color
+    // call function to display product information
     getProductInfo(event.target.id);     
 }
 
-// Function to remove opacity from image when no longer hovered over
+// function to remove opacity and hide product information when image is no longer hovered over
 function removeOpacity(event) {
-    // Remove appropriate CSS class
+    // remove "opacity" CSS class from image
     event.target.classList.remove("opacity");
-    // Clear product information displayed
-    clearProductInfo();
-    event.preventDefault();    
-}
-
-// Function to retrieve and display product information
-function getProductInfo(paintColor) {
-    // Get color name from paintColor ID
-    let colorName = paintColor.substr(2);
-    // Get price from paintPrices object
-    let price = paintPrices[colorName];
-    // Update product information on page
-    updateProductInfo(colorName, price);
-}
-
-// Function to clear product information displayed
-function clearProductInfo() {
+    // clear product information from the page
     let element = document.getElementById('color-price');
     element.textContent = '';
     let color = document.getElementById('color-name');
     color.textContent = ''; 
+//This line of code makes sure that the browser does not follow the link when the mouse pointer moves out of the image.
+    event.preventDefault();    
 }
 
-// Function to update product information on page
-function updateProductInfo(colorName, price) {
-    // Select element to display color name
-    let color = document.getElementById('color-name');
-    // Display color name
-    color.textContent = colorName;
-    // Select element to display price
-    let colorPrice = document.getElementById('color-price');
-    // Display price
-    colorPrice.textContent = '$' + price;
+// function to display product information based on hovered over image's id
+function getProductInfo(paintColor) {
+    let price;
+    let colorName;  
+    
+    switch (paintColor) {
+ // set variables for price and color name and invoke a function to update the price
+        case 'pn1':           
+            price = 14.99;
+            colorName = "Lim Gre";
+            updatePrice(colorName, price);     
+            break;           
+// set variables for price and color name and invoke a function to update the price
+            case 'pn2':
+            price = 11.14;
+            colorName = "Med Bro";
+            updatePrice(colorName, price);  
+            break; 
+ // set variables for price and color name and invoke a function to update the price           
+        case 'pn3':
+            price = 22.99;
+            colorName = "Roy Blu";
+            updatePrice(colorName, price);  
+            break;   
+// set variables for price and color name and invoke a function to update the price
+        case 'pn4':
+            price = 4.99;
+            colorName = "Sol Bla";
+            updatePrice(colorName, price);  
+            break;   
+// set variables for price and color name and invoke a function to update the price
+        case 'pn5':
+            price = 8.22;
+            colorName = "Sol Cya";
+            updatePrice(colorName, price);  
+            break;   
+// set variables for price and color name and invoke a function to update the price
+        case 'pn6':
+            price = 11.99;
+            colorName = "Sol Purp";
+            updatePrice(colorName, price);  
+            break;   
+// set variables for price and color name and invoke a function to update the price
+        case 'pn7':
+            price = 13.42;
+            colorName = "Sol Red";
+            updatePrice(colorName, price);  
+            break;   
+// set variables for price and color name and invoke a function to update the price
+        case 'pn8':
+            price = 21.98;
+            colorName = "Sol Whi";
+            updatePrice(colorName, price);  
+            break;   
+// set variables for price and color name and invoke a function to update the price
+        case 'pn9':
+            price = 14.99;
+            colorName = "Sol Yel";
+            updatePrice(colorName, price);  
+            break;   
+          default:              
+    }
+
+    function updatePrice(colorName, price)
+    {       
+        let colorPrice = document.getElementById('color-price');// select element with corresponding id
+        // display price
+        colorPrice.textContent = price
+        let color = document.getElementById('color-name');//select element with corresponding name
+        //display color name
+        color.textContent = colorName;
+    }
+    
 }
