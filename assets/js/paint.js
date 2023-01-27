01ed5349c6163d1c8b89489d97ad42f5757094d2
JOSE ALARCON MOLINA <jos3.4larc0n@gmail.com>
2023-01-27 00:20:16 -0500

    define removeOpacity addOpacity functions pass information to display in getProductInfo

--- assets/js/paint.js
+++ assets/js/paint.js
@@ -1,72 +1,72 @@
+// Declare pricing information for each paint color
+const paintPrices = {
+    'Lime Green': 14.99,
+    'Medium Brown': 11.14,
+    'Royal Blue': 22.99,
+    'Solid Black': 4.99,
+    'Solid Cyan': 8.22,
+    'Solid Purple': 11.99,
+    'Solid Red': 13.42,
+    'Solid White': 21.98,
+    'Solid Yellow': 14.99
+};
+
+// Function to configure event listeners for image elements
 function configureListeners() {
-    let images = document.querySelectorAll("img");  // select img elements  
+    // Select all img elements
+    let images = document.querySelectorAll("img");  
+    // Iterate through each image element
     images.forEach(function(image) {
-        // add mouseover event listener
+        // Add mouseover event listener
         image.addEventListener("mouseover", addOpacity);
-        // add mouseout event listener
+        // Add mouseout event listener
         image.addEventListener("mouseout", removeOpacity);
     });
 }
 
+// Function to add opacity to image when hovered over
 function addOpacity(event) {
-    // add appropriate CSS class
+    // Add appropriate CSS class
+    event.target.classList.add("opacity");
+    // Get product information for hovered over color
     getProductInfo(event.target.id);     
 }
 
+// Function to remove opacity from image when no longer hovered over
 function removeOpacity(event) {
-     //remove appropriate CSS class
-
-    let element = document.getElementById('color-price');
-        element.textContent = '';
-        
-    let color = document.getElementById('color-name');
-        color.textContent = ''; 
-
+    // Remove appropriate CSS class
+    event.target.classList.remove("opacity");
+    // Clear product information displayed
+    clearProductInfo();
     event.preventDefault();    
 }
 
+// Function to retrieve and display product information
 function getProductInfo(paintColor) {
-    let price;
-    let colorName;  
-    
-    switch (paintColor) {
-        case 'pn1':           
-            // set variables for price and color name and invoke a function to update the price     
-            break;           
-        case 'pn2':
-            // set variables for price and color name and invoke a function to update the price    
-            break;            
-        case 'pn3':
-            // set variables for price and color name and invoke a function to update the price  
-            break;   
-        case 'pn4':
-            // set variables for price and color name and invoke a function to update the price  
-            break;   
-        case 'pn5':
-            // set variables for price and color name and invoke a function to update the price       
-            break;   
-        case 'pn6':
-            // set variables for price and color name and invoke a function to update the price        
-            break;   
-        case 'pn7':
-            // set variables for price and color name and invoke a function to update the price 
-            break;   
-        case 'pn8':
-            // set variables for price and color name and invoke a function to update the price   
-            break;   
-        case 'pn9':
-            // set variables for price and color name and invoke a function to update the price 
-            break;   
-          default:              
-    }
+    // Get color name from paintColor ID
+    let colorName = paintColor.substr(2);
+    // Get price from paintPrices object
+    let price = paintPrices[colorName];
+    // Update product information on page
+    updateProductInfo(colorName, price);
+}
 
-    function updatePrice(colorName, price)
-    {       
-        let colorPrice = // select element with corresponding id
-        // display price
-        
-        let color = // select element with corresponding id
-        //display color name
-    }
-    
+// Function to clear product information displayed
+function clearProductInfo() {
+    let element = document.getElementById('color-price');
+    element.textContent = '';
+    let color = document.getElementById('color-name');
+    color.textContent = ''; 
+}
+
+// Function to update product information on page
+function updateProductInfo(colorName, price) {
+    // Select element to display color name
+    let color = document.getElementById('color-name');
+    // Display color name
+    color.textContent = colorName;
+    // Select element to display price
+    let colorPrice = document.getElementById('color-price');
+    // Display price
+    colorPrice.textContent = '$' + price;
 }
